edf207b9be58c9ed61918e126deade0c5bc4fcb6
Pablit0rg <pablorosagomes@gmail.com>
2025-12-14 13:30:07 -0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/sections/hero'
+import Services from '@/components/sections/services'
+import About from '@/components/sections/about'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <About />
+    </>
+  )
+}
